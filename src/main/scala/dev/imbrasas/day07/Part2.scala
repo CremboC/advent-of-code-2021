deee3bf276f11aa9eed02b57c9cfc0ff5634689d
Paulius Imbrasas <cremboc@gmail.com>
2021-12-07 18:56:13 +0000

    Rename function because I forgot maths

--- src/main/scala/dev/imbrasas/day07/Part2.scala
+++ src/main/scala/dev/imbrasas/day07/Part2.scala
@@ -8,7 +8,7 @@ import scala.annotation.tailrec
 
 object Part2 extends IOApp.Simple:
   extension (i: Int)
-    def factorial: Int =
+    def triangular: Int =
       @tailrec def loop(n: Int, acc: Int): Int =
         if n == 0 then acc
         else loop(n - 1, n + acc)
@@ -25,7 +25,7 @@ object Part2 extends IOApp.Simple:
 
       val answer = (minPos to maxPos).map { moveTo =>
         crabs.map { pos =>
-          (moveTo.max(pos) - moveTo.min(pos)).factorial
+          (moveTo.max(pos) - moveTo.min(pos)).triangular
         }.sum
       }.min
 
