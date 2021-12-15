c106cff5f98a03abee7d6e12b4f1fb37f1738042
Paulius Imbrasas <cremboc@gmail.com>
2021-12-15 09:14:17 +0000

    Day 13, both parts

--- src/main/scala/dev/imbrasas/day13/Day13.scala
+++ src/main/scala/dev/imbrasas/day13/Day13.scala
@@ -13,39 +13,29 @@ import scala.collection.immutable.Queue
 case class Point(x: Int, y: Int)
 
 case class Grid(grid: Vector[Vector[Boolean]]):
-  lazy val midY = grid.length / 2.0
   def set(x: Int, y: Int, v: Boolean): Grid =
     copy(grid = grid.updated(y, grid(y).updated(x, v)))
 
   def foldAtY(y: Int): Grid =
-    println(s"fy y=${y} l=${grid.length} mid=${grid.length / 2.0}")
-
     val top = grid.take(y)
-    val bottom = grid.drop(y).reverse
-    println(
-      s"top=${top.length} bottom=${bottom.length} sum=${top.length + bottom.length}"
-    )
-    val grid_ = top.zip(bottom).map { (l, r) =>
-      val diff = l.length - r.length
-      r.zipWithIndex.foldLeft(l) { case (acc, (el, idx)) =>
-        val idx_ = (idx + diff).max(0)
-        acc.updated(idx_, el || acc(idx_))
+    val bottom = grid.drop(y + 1).reverse
+    val isMidpoint = grid.length / 2.0 == y
+    val bottom_ =
+      if isMidpoint then
+        bottom.prependedAll(Vector(Vector.fill(grid.head.length)(false)))
+      else bottom
+    val grid_ = top.zip(bottom_).map { (l, r) =>
+      l.mapWithIndex { (el, idx) =>
+        r(idx) || el
       }
     }
     copy(grid_)
 
   def foldAtX(x: Int): Grid =
-    println(s"fx x=${x} l=${grid.head.length} mid=${grid.head.length / 2.0}")
-    val split = grid.map(row => (row.take(x), row.drop(x).reverse))
-    println(
-      s"top=${split.head._1.length} bottom=${split.head._2.length}, sum=${split.head._1.length + split.head._2.length}"
-    )
+    val split = grid.map(row => (row.take(x), row.drop(x + 1).reverse))
     val grid_ = split.map { (l, r) =>
-      val diff = l.length - r.length
-      println(s"diff=${diff}")
-      r.zipWithIndex.foldLeft(l) { case (acc, (el, idx)) =>
-        val idx_ = (idx + diff).max(0)
-        acc.updated(idx_, el || acc(idx_))
+      l.mapWithIndex { (el, idx) =>
+        r(idx) || el
       }
     }
 
@@ -55,18 +45,12 @@ case class Grid(grid: Vector[Vector[Boolean]]):
 
   override def toString: String =
     grid
-      .map(_.map(if _ then "█" else "░").mkString(""))
+      .map(_.map(if _ then "#" else ".").mkString(""))
       .mkString("\n") + s"\n"
 
 object Grid:
   def of(x: Int, y: Int): Grid =
-    Grid(
-      (0 to y).map { y_ =>
-        (0 to x).map { x_ =>
-          false
-        }.toVector
-      }.toVector
-    )
+    Grid(Vector.fill(y + 1, x + 1)(false))
 
 object Day13 extends IOApp.Simple:
 
@@ -74,7 +58,7 @@ object Day13 extends IOApp.Simple:
     async[IO] {
       val input =
         Input
-          .lines("day13_sample.txt")
+          .lines("day13.txt")
           .compile
           .toList
           .await
@@ -96,26 +80,17 @@ object Day13 extends IOApp.Simple:
         acc.set(point.x, point.y, true)
       }
 
-      // IO.println(start).await
-      // IO.println(start.foldAtX(4)).await
-      // IO.println(start.foldAtX(8).foldAtY(8)).await
-      // IO.println(start.foldAtY(2).foldAtX(2)).await
-
-      // val part1 = folds.head match
-      //   case ("x", n) => start.foldAtX(n)
-      //   case ("y", n) => start.foldAtY(n)
-      // IO.println(part1.dotCount).await
+      val part1 = folds.head match
+        case ("x", n) => start.foldAtX(n)
+        case ("y", n) => start.foldAtY(n)
+      IO.println(part1.dotCount).await
 
       val s = IO.realTime.await
 
       val part2 =
         folds.foldLeft(start) {
-          case (acc, ("x", n)) =>
-            val acc_ = acc.foldAtX(n)
-            acc_
-          case (acc, ("y", n)) =>
-            val acc_ = acc.foldAtY(n)
-            acc_
+          case (acc, ("x", n)) => acc.foldAtX(n)
+          case (acc, ("y", n)) => acc.foldAtY(n)
         }
       val e = IO.realTime.await
 
