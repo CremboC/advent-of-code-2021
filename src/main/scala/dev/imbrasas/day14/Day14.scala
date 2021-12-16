97779b8ee8c54f0cb09e909ff56c7481a5cc95d7
Paulius Imbrasas <cremboc@gmail.com>
2021-12-16 08:22:44 +0000

    Day 14, part 1

--- src/main/scala/dev/imbrasas/day14/Day14.scala
+++ src/main/scala/dev/imbrasas/day14/Day14.scala
@@ -10,70 +10,54 @@ import dev.imbrasas.util.{Input, Resources}
 import scala.annotation.tailrec
 import scala.collection.immutable.Queue
 
-case class Rule(pair: String, insert: String)
+case class Rule(pair: String, insert: Char)
 
 object Day14 extends IOApp.Simple:
 
+  extension (s: String)
+    def allIndexOff(str: String): Queue[Int] =
+      val length = s.length
+      var i = 1
+      var acc = Queue.newBuilder[Int]
+      while i < length do
+        val substr = s.substring(i - 1, i + 1)
+        if substr == str then acc.addOne(i - 1)
+        i += 1
+
+      acc.result
+
   override def run: IO[Unit] =
     async[IO] {
-      val input =
-        Input
-          .lines("day14_sample.txt")
-          .compile
-          .toList
-          .await
+      val input = Input.lines("day14_sample.txt").compile.toList.await
 
       val template = input.head
       val rules = input
         .drop(2)
         .map { string =>
           val Array(pair, insert) = string.split(" -> ")
-          (pair, insert.head)
+          Rule(pair, insert.head)
         }
-        .toMap
 
-      val maxSteps = 1
+      val maxSteps = 40
 
       @tailrec
-      def solve(step: Int, template: String, rules: Map[String, Char]): String =
-        println((step, template, template.length))
+      def solve(step: Int, template: String, rules: List[Rule]): String =
+        println((step, template.length))
         if step == maxSteps then template
         else
-          val template_ = template
-            .sliding(2)
-            .foldLeft(StringBuilder()) { (acc, pair) =>
-              println(("sliding", acc, pair, rules.get(pair)))
-              rules.get(pair) match
-                case Some(insert) =>
-                  println(("inserting", StringBuilder(pair).insert(1, insert)))
-                  acc.append(StringBuilder(pair).insert(1, insert))
-                case None =>
-                  acc.append(pair)
+          val template_ = rules
+            .flatMap { rule =>
+              val idx = template.allIndexOff(rule.pair)
+              idx.map(i => (i + 1, rule))
+            }
+            .sortBy((idx, s) => idx)(Ordering[Int].reverse)
+            .foldLeft(StringBuilder(template)) { case (acc, (idx, rule)) =>
+              acc.insert(idx, rule.insert)
             }
             .toString
-          // .foldLeft(StringBuilder(template)) { case (acc, pair) =>
-          //   val ((p1, i)) :: ((p2, j)) :: Nil = pair
-          //   rules.get(s"${p1}${p2}") match
-          //     case Some(i) => acc.insert(j, i)
-          // }
-
-          // val template_ = rules
-          //   .mapFilter { rule =>
-          //     val idx = template.indexOf(rule.pair)
-          //     if idx == -1 then None
-          //     else Some((idx + 1, rule))
-          //   }
-          //   .sortBy((idx, s) => idx)(Ordering[Int].reverse)
-          //   .foldLeft(StringBuilder(template)) { case (acc, (idx, rule)) =>
-          //     println(s"inserting $rule at $idx")
-          //     acc.insert(idx, rule.insert)
-          //   }
-          //   .toString
           solve(step + 1, template_, rules)
 
-      val part1Answer = solve(0, template, rules)
-      IO.println(part1Answer).await
-
-      // expected: NBBBCNCCNBBNBNBBCHBHHBCHB
-      // actual:   NBBBCNCCNBNBBCHBHHBCB
+      val part1Template = solve(0, template, rules)
+      val map = part1Template.groupMapReduce(identity)(_ => 1L)(_ + _)
+      IO.println(map.maxBy(_._2)._2 - map.minBy(_._2)._2).await
     }
